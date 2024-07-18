f0a9eb328406dd95741effd31e1770e65be8f9d8
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-07-18 09:16:21 +0900

    Update GuestbookController.java

--- ex4/src/main/java/com/example/ex4/controller/GuestbookController.java
+++ ex4/src/main/java/com/example/ex4/controller/GuestbookController.java
@@ -3,8 +3,6 @@ package com.example.ex4.controller;
 
 import com.example.ex4.dto.GuestbookDTO;
 import com.example.ex4.dto.PageRequestDTO;
-import com.example.ex4.dto.PageResultDTO;
-import com.example.ex4.entity.Guestbook;
 import com.example.ex4.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -25,6 +23,7 @@ public class GuestbookController {
 
   @GetMapping({"", "/", "/list"})
   public String list(Model model, PageRequestDTO pageRequestDTO) {
+    // PageRequestDTO는 커맨드 객체이며, 다음페이지에 model로 전송안해도 넘어간다.
     // 페이지에 대한 요청 정보를 통해서 최종 PageResultDTO 생성
     model.addAttribute("pageResultDTO", guestbookService.getList(pageRequestDTO));
     return "/guestbook/list";
@@ -39,38 +38,41 @@ public class GuestbookController {
   public String registerPost(GuestbookDTO guestbookDTO, RedirectAttributes ra) {
     log.info("register post........");
     Long gno = guestbookService.register(guestbookDTO);
-    ra.addFlashAttribute("msg", gno+"번이 등록");
+    ra.addFlashAttribute("msg", gno + "번이 등록");
     // redirect는 컨트롤러로 재전송한다는 의미
     return "redirect:/guestbook/list";
   }
 
   @GetMapping({"/read", "/modify"})
-  public void read(Long gno, int page, Model model) {
+  public void read(Long gno, PageRequestDTO pageRequestDTO, Model model) {
     GuestbookDTO guestbookDTO = guestbookService.read(gno);
     model.addAttribute("guestbookDTO", guestbookDTO);
-    model.addAttribute("page", page);
   }
 
   @PostMapping("/modify")
-  public String modify(GuestbookDTO guestbookDTO, PageRequestDTO pageRequestDTO
-      , RedirectAttributes ra) {
+  public String modify(GuestbookDTO guestbookDTO, PageRequestDTO pageRequestDTO,
+                       RedirectAttributes ra) {
     guestbookService.modify(guestbookDTO);
-    ra.addFlashAttribute("msg",guestbookDTO.getGno()+"번이 수정");
-    ra.addAttribute("page",pageRequestDTO.getPage());
-    ra.addAttribute("gno",guestbookDTO.getGno());
+    ra.addFlashAttribute("msg", guestbookDTO.getGno() + "번이 수정");
+    ra.addAttribute("page", pageRequestDTO.getPage());
+    ra.addAttribute("type", pageRequestDTO.getType());
+    ra.addAttribute("keyword", pageRequestDTO.getKeyword());
+    ra.addAttribute("gno", guestbookDTO.getGno());
     return "redirect:/guestbook/read";
   }
-  @PostMapping("/remove")
-  public String remove(GuestbookDTO guestbookDTO, PageRequestDTO pageRequestDTO
-      , RedirectAttributes ra) {
-    guestbookService.remove(guestbookDTO);
-    if(guestbookService.getList(pageRequestDTO).getDtoList().size() ==0
-        && pageRequestDTO.getPage() != 1){
-      pageRequestDTO.setPage(pageRequestDTO.getPage()-1);
-    }
 
-    ra.addFlashAttribute("msg",guestbookDTO.getGno()+"번이 삭제");
-    ra.addAttribute("page",pageRequestDTO.getPage());
+  @PostMapping("/remove")
+  public String remove(GuestbookDTO guestbookDTO, PageRequestDTO pageRequestDTO,
+                       RedirectAttributes ra) {
+    guestbookService.remove(guestbookDTO);
+    if (guestbookService.getList(pageRequestDTO).getDtoList().size() == 0 &&
+        pageRequestDTO.getPage() != 1) {
+      pageRequestDTO.setPage(pageRequestDTO.getPage() - 1);
+    }
+    ra.addFlashAttribute("msg", guestbookDTO.getGno() + "번이 삭제");
+    ra.addAttribute("page", pageRequestDTO.getPage());
+    ra.addAttribute("type", pageRequestDTO.getType());
+    ra.addAttribute("keyword", pageRequestDTO.getKeyword());
     return "redirect:/guestbook/list";
   }
 }
