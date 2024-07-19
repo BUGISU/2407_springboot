469c9c0569b1f8f5088d1067a63e87a3e6d023b3
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-07-19 16:38:11 +0900

    20240719

--- ex5/src/main/java/com/example/ex5/entity/Board.java
+++ ex5/src/main/java/com/example/ex5/entity/Board.java
@@ -1,4 +1,23 @@
 package com.example.ex5.entity;
 
-public class Board extends BasicEntity{
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "writer") //항상 조인되어있는 관계를 느슨하게 만들어줌
+public class Board extends BasicEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long bno;
+
+  private String title;
+  private String content;
+
+  @ManyToOne( fetch = FetchType.LAZY) //속도를 좀 빠르게 만들어줌
+  private Member writer;
 }
