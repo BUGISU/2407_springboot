e5cb5fd1b95fb98c3410bd55a96d107ae9235859
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-07-30 15:39:15 +0900

    20240730

--- ex6/src/main/java/com/example/ex6/repository/MovieImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ex6.repository;
-
-import com.example.ex6.entity.MovieImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieImageRepository extends JpaRepository<MovieImage, Long> {
-
-}
