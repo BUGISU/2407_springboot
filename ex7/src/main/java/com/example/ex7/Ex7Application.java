a3d3929bdc3e3f84e07ae3b805d9f52e3b9f44a2
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-08-06 16:41:42 +0900

    20240806

--- ex7/src/main/java/com/example/ex7/Ex7Application.java
+++ ex7/src/main/java/com/example/ex7/Ex7Application.java
@@ -2,12 +2,18 @@ package com.example.ex7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories //basicEntity를 사용하기 위함
 public class Ex7Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ex7Application.class, args);
+		System.out.println("http://localhost:8080/ex7");
 	}
+
 
 }
