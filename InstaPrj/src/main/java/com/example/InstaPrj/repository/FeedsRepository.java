fef8ecda0c35bc0c7b56a8422fad9cefda206969
이니 <38304918+JISUSAMA@users.noreply.github.com>
2024-08-13 23:12:36 +0900

    Update FeedsRepository.java

--- InstaPrj/src/main/java/com/example/InstaPrj/repository/FeedsRepository.java
+++ InstaPrj/src/main/java/com/example/InstaPrj/repository/FeedsRepository.java
@@ -12,26 +12,26 @@ public interface FeedsRepository {
 //  group by m.mno;
 
     // 영화에 대한 리뷰의 평점과 댓글 갯수를 출력
-    @Query("select m, avg(coalesce(r.grade, 0)), count(distinct r) " +
-            "from Movie m left outer join Review r on r.movie=m group by m ")
+    @Query("select f, avg(coalesce(r.grade, 0)), count(distinct r) " +
+            "from Feeds f left outer join Reviews r on r.feeds=f group by f ")
     Page<Object[]> getListPage(Pageable pageable);
 
     // 아래와 같은 경우 mi를 찾기 위해서 review 카운트 만큼 반복횟수도 늘어나는 문제점
     // mi의 inum이 가장 낮은 이미지 번호가 출력된다.
     // 영화와 영화이미지,리뷰의 평점과 댓글 갯수 출력
-    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m " +
-            "left outer join MovieImage mi on mi.movie = m " +
-            "left outer join Review     r  on r.movie  = m group by m ")
+    @Query("select f, pt, avg(coalesce(r.grade, 0)), count(distinct r) from Feeds f " +
+            "left outer join Photos pt on pt.feeds = f " +
+            "left outer join Reviews   r  on r.feeds  = f group by f ")
     Page<Object[]> getListPageImg(Pageable pageable);
 
     // spring 3.x에서는 실행 안됨.
-    @Query("select m,max(mi),avg(coalesce(r.grade, 0)),count(distinct r) from Movie m " +
-            "left outer join MovieImage mi on mi.movie = m " +
-            "left outer join Review     r  on r.movie  = m group by m ")
+    @Query("select f,max(pt),avg(coalesce(r.grade, 0)),count(distinct r) from Feeds f " +
+            "left outer join Photos pt on pt.feeds = f " +
+            "left outer join Reviews   r  on r.feeds  = f group by f ")
     Page<Object[]> getListPageMaxImg(Pageable pageable);
 
     // Native Query = SQL
-    @Query(value = "select m.mno, mi.inum, mi.img_name, " +
+    @Query(value = "select f.fno, pt.pnum, pt.imgName, " +
             "avg(coalesce(r.grade, 0)), count(r.reviewnum) " +
             "from db7.movie_image mi left outer join db7.movie m on m.mno=mi.movie_mno " +
             "left outer join db7.review r on m.mno=r.movie_mno " +
@@ -56,5 +56,4 @@ public interface FeedsRepository {
             "left outer join Review r on r.movie = m " +
             "where m.mno = :mno group by mi ")
     List<Object[]> getMovieWithAll(Long mno); //특정 영화 조회
-    .
 }
